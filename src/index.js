7f24578d9891f3765df65664417a24596a7a2ab4
Christopher <christopher.kim43@gmail.com>
2022-03-01 13:20:21 -0500

    Complete up to priority with color.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form');
+  const list = document.querySelector('#tasks');
+
+  let addPriority = document.createElement('select');
+  let opt1 = document.createElement('option')
+  let opt2 = document.createElement('option')
+  let opt3 = document.createElement('option')
+
+  opt1.text = 'High'
+  opt2.text = 'Medium'
+  opt3.text = 'Low'
+
+  opt1.value = 'high'
+  opt2.value = 'medium'
+  opt3.value = 'low'
+
+  form.appendChild(addPriority);
+
+  addPriority.add(opt1)
+  addPriority.add(opt2)
+  addPriority.add(opt3)
+
+  
+  
+const option = document.querySelector('option')
+
+function targetColor(){
+  if (addPriority.value === 'high'){
+    return 'red'
+  } else if (addPriority.value === 'medium'){
+    return 'yellow'
+  } else if (addPriority.value === 'low'){
+    return 'green'
+  }
+}
+  
+  
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const targetValue = e.target["new-task-description"].value
+    toDoAction(targetValue);
+    
+    
+  
+  })
+
+  function toDoAction (action){
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+    btn.addEventListener('click', deleteAction)
+    p.textContent = `${action} `;
+    p.style.color = targetColor();
+    list.appendChild(p);
+    btn.textContent = 'x';
+    p.appendChild(btn);
+    
+  
+  }
+  
+  function deleteAction(e) {
+    e.target.parentNode.remove();
+  }  
+
+  
+}
+);
+
+
+
+
